3f4cdcbfe27819aab338a2409500a76c36b63c69
bryanchrist <brc4cb@virginia.edu>
2023-06-21 14:55:58 -0400

    updating generator script

--- generate/adapter.py
+++ generate/adapter.py
@@ -23,7 +23,7 @@ from scripts.prepare_alpaca import generate_prompt
 
 
 def main(
-    prompt: str = "What food do lamas eat?",
+    prompt: str = "Write a grade 4 Multiplication question and corresponding equation to solve the problem.",
     input: str = "",
     adapter_path: Path = Path("out/adapter/ASDiv/lit_model_adapter_finetuned.pth"),
     checkpoint_dir: Path = Path("checkpoints/tiiuae/falcon-40b"),
